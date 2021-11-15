988cd9a7c2d37d8dafdb35bfd262fe0dc9fd5b8d
unnat <53619178+unnat404@users.noreply.github.com>
2021-11-16 00:01:46 +0530

    Revert "added product details & created user(for logged-in) from admin dashboard"
    This reverts commit 8a27e1f9a7e3e0088d9c948afbd911532e93cf25.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -89,23 +89,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DEMO_TEST',
-        'USER': 'postgres' ,
-        'PASSWORD': 'hello@POSTGRES123' ,
-        'HOST': 'localhost' ,
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
