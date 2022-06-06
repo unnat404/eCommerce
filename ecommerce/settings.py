019aa455788b86592fd432577215b94f6b37f4bf
unnat <53619178+unnat404@users.noreply.github.com>
2022-06-06 10:37:53 +0530

    heroku dpeloy #1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -36,9 +36,13 @@ SECRET_KEY = config('SECRET_KEY')
 # ===========================================================================================================
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -57,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,10 +5,15 @@ from django.conf.urls.static import static
 from django.conf import settings
 # ... the rest of your URLconf here ...
 
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 # urlpatterns += staticfiles_urlpatterns() #add kiye hai delete maar dena (not good for Production)
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  # THIS WAS IN TUTORIAL
